bb56b0f2e87e08d1799122bc9ea682c13e6ac286
mohammedf16X <fivehjoftoj366@gmail.com>
2025-03-17 04:24:07 +0300

    Update Speedscript.lua

--- Speedscript.lua
+++ Speedscript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourRepo/YourScript/speed_script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammedf16X/Scriptspeednew2/main/Speedscript.lua"))()
